5db730ebd6ba79bc226b7c91bab18d124eeb126e
Unknown <luigiaiello97@gmail.com>
2018-08-08 14:27:35 +0200

    Added pods
    - SkyFloatingLabelTextField
    - Moya
    - SwiftyJSON

--- Podfile
+++ Podfile
@@ -12,6 +12,18 @@ def graphics_components_pods
 	pod 'SkyFloatingLabelTextField'
 end
 
+def networking_pods
+    pod 'Moya'
+end
+
+def utilites_pods
+    pod 'SwiftyJSON'
+end
+
 target 'Iliad' do
 	graphics_components_pods
+	networking_pods
+	utilites_pods
 end
+
+
--- Pods/Target Support Files/Pods-Iliad/Pods-Iliad-frameworks.sh
+++ Pods/Target Support Files/Pods-Iliad/Pods-Iliad-frameworks.sh
@@ -143,10 +143,18 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Moya/Moya.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Result/Result.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SkyFloatingLabelTextField/SkyFloatingLabelTextField.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Moya/Moya.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Result/Result.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SkyFloatingLabelTextField/SkyFloatingLabelTextField.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
