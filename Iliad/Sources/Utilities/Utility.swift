db16e05ed2e41da4ad89a0936ecbf134a24b9bf0
Unknown <luigiaiello97@gmail.com>
2018-08-17 15:30:03 +0200

    Syntax improvement

--- Iliad/Sources/Utilities/Utility.swift
+++ Iliad/Sources/Utilities/Utility.swift
@@ -34,7 +34,7 @@ class Utility {
     static func isValidEmail(testStr: String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
 
-        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
         return emailTest.evaluate(with: testStr)
     }
 }
