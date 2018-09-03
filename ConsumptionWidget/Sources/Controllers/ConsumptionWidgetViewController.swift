5bc1f37056c2e0debf1ed899b21f16614fa4fd4b
Unknown <luigiaiello97@gmail.com>
2018-09-03 18:06:57 +0200

    Added widget

--- ConsumptionWidget/Sources/Controllers/ConsumptionWidgetViewController.swift
+++ ConsumptionWidget/Sources/Controllers/ConsumptionWidgetViewController.swift
@@ -13,18 +13,30 @@ class ConsumptionWidgetViewController: UIViewController, NCWidgetProviding {
 
     // Mark - Outlets
     @IBOutlet weak var callsLabel: UILabel!
+    @IBOutlet weak var callsImageView: UIImageView!
+
     @IBOutlet weak var SMSLabel: UILabel!
+    @IBOutlet weak var SMSImageView: UIImageView!
+
     @IBOutlet weak var MMSLabel: UILabel!
+    @IBOutlet weak var MMSImageView: UIImageView!
+
     @IBOutlet weak var dataLabel: UILabel!
+    @IBOutlet weak var dataImageView: UIImageView!
+
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var loginButton: CustomButton!
 
     // Mark - Variables
     let userDefaults = UserDefaults(suiteName: "group.com.luigiaiello.consumptionWidget")
+    let mainApp: String = "areapersonale://"
 
     // Mark - Override
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        _ = updateData()
+        configurationUI()
+        configurationText()
     }
     
     override func didReceiveMemoryWarning() {
@@ -32,10 +44,20 @@ class ConsumptionWidgetViewController: UIViewController, NCWidgetProviding {
     }
     
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        guard
+            let loginSuccessful = userDefaults?.value(forKey: "kLoginSuccessful") as? Bool,
+            loginSuccessful
+        else {
+            showLoginMessage()
+            completionHandler(NCUpdateResult.failed)
+            return
+        }
+        loginButton.isHidden = true
         guard updateData() else {
             completionHandler(NCUpdateResult.noData)
             return
         }
+        stackView.isHidden = false
         // Perform any setup necessary in order to update the view.
         
         // If an error is encountered, use NCUpdateResult.Failed
@@ -45,7 +67,27 @@ class ConsumptionWidgetViewController: UIViewController, NCWidgetProviding {
         completionHandler(NCUpdateResult.newData)
     }
 
+    // Mark - Setup
+    private func configurationUI() {
+        callsImageView.tintColor = .iliadRed
+        SMSImageView.tintColor = .iliadRed
+        MMSImageView.tintColor = .iliadRed
+        dataImageView.tintColor = .iliadRed
+    }
+    private func configurationText() {
+        loginButton.setTitle("Extension" ~> "LOGIN_BUTTON", for: .normal)
+        callsLabel.text = "--"
+        SMSLabel.text = "--"
+        MMSLabel.text = "--"
+        dataLabel.text = "--"
+    }
+
     // Mark - Helpers
+    private func showLoginMessage() {
+        stackView.isHidden = true
+        loginButton.isHidden = false
+    }
+
     private func updateData() -> Bool {
         guard
             let calls = userDefaults?.value(forKey: "kCalls") as? String,
@@ -61,5 +103,16 @@ class ConsumptionWidgetViewController: UIViewController, NCWidgetProviding {
         dataLabel.text = data
 
         return true
+    }
+
+    // Mark - Actions
+    @IBAction func loginDidTap(_ sender: Any) {
+        guard let url = URL(string: mainApp) else {
+            return
+        }
+
+        extensionContext?.open(url, completionHandler: { (success) in
+            print(success)
+        })
     }
 }
